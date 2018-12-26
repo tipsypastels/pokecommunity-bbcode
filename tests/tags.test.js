7dccdb621527d8183c4ebc4011a1ce700c4349cd
tipsypastels <dakotasankey@gmail.com>
2018-12-25 21:39:58 -0800

    [fa] and param parsing

--- tests/tags.test.js
+++ tests/tags.test.js
@@ -16,6 +16,21 @@ tagTest('bbcode', {
   html: 'yo',
 });
 
+tagTest('i', {
+  bbcode: '[i]hello[/i]',
+  html: '<em>hello</em>',
+});
+
+tagTest('fa (no param)', {
+  bbcode: '[fa]gifts[/fa]',
+  html: '<i class="fas fa-gifts"></i>',
+});
+
+tagTest('fa (with param)', {
+  bbcode: '[fa=fab]twitter[/fa]',
+  html: '<i class="fab fa-twitter"></i>',
+});
+
 // TODO these are the correct characters but uh
 // not sure why we get the output like this :/
 tagTest('noparse', {
